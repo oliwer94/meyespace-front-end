32b425332fd6e1c48674dc2a16e6b47787eb0b1d
oliwer94 <oliwer94@gmail.com>
2017-05-12 22:21:45 +0200

    some stuff

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -2,6 +2,8 @@
 import { Router } from '@angular/router';
 
 import { AlertService, UserService } from '../_services/index';
+import * as $ from 'jquery';
+var el: JQuery;
 
 @Component({
     moduleId: module.id,
@@ -15,19 +17,20 @@ export class RegisterComponent {
     constructor(
         private router: Router,
         private userService: UserService,
-        private alertService: AlertService) { }
+        private alertService: AlertService) { }        
 
     register() {
         this.loading = true;
+        //$('#flagstrap-1f5Ao0Jo').val();
         this.userService.create(this.model)
             .subscribe(
-                data => {
-                    this.alertService.success('Registration successful. Please check you emails to verify your account!', true);
-                    this.router.navigate(['/login']);
-                },
-                error => {
-                    this.alertService.error(error);
-                    this.loading = false;
-                });
+            data => {
+                this.alertService.success('Registration successful. Please check you emails to verify your account!', true);
+                this.router.navigate(['/login']);
+            },
+            error => {
+                this.alertService.error(error);
+                this.loading = false;
+            });
     }
 }
